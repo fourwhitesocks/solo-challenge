1c618d1d630b7bdeb1e7451c42d7b6c85faeb443
amy stout <asbilly92@gmail.com>
2022-12-07 19:11:46 -0500

    style: :lipstick: added a temp custom class for limiting title limit on large screens

--- src/lib/components/Title.svelte
+++ src/lib/components/Title.svelte
@@ -2,6 +2,14 @@
   export let title = 'My Longest Sail';
 </script>
 
-<h1 class="text-4xl font-semibold text-gray-900 dark:text-white md:text-5xl xl:text-6xl">
+<h1
+  class="title-ch-limit text-4xl font-semibold text-gray-900 dark:text-white md:text-5xl xl:text-6xl"
+>
   {title}
 </h1>
+
+<style>
+  .title-ch-limit {
+    max-width: 8ch;
+  }
+</style>
