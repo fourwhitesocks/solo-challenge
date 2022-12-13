b147137256a7c2c3efc4ad831ce13ffec364b4d3
amy stout <asbilly92@gmail.com>
2022-12-13 08:25:54 -0500

    style: :lipstick: changed some styles for light/dark mode

--- src/routes/notes/+page.svelte
+++ src/routes/notes/+page.svelte
@@ -6,6 +6,21 @@
   <a href="/notes" data-sveltekit-prefetch>Notes</a>
 </nav>
 
+<picture>
+  <img
+    sizes="(max-width: 1400px) 100vw, 1400px"
+    srcset="
+  start-line-no-kite-op_yyxdi7_c_scale,w_200.webp 200w,
+  start-line-no-kite-op_yyxdi7_c_scale,w_535.webp 535w,
+  start-line-no-kite-op_yyxdi7_c_scale,w_973.webp 973w,
+  start-line-no-kite-op_yyxdi7_c_scale,w_1003.webp 1003w,
+  start-line-no-kite-op_yyxdi7_c_scale,w_1358.webp 1358w,
+  start-line-no-kite-op_yyxdi7_c_scale,w_1400.webp 1400w"
+    src="start-line-no-kite-op_yyxdi7_c_scale,w_1400.webp"
+    alt=""
+  />
+</picture>
+
 <style>
   nav {
     display: flex;
--- src/routes/posts/[slug]/+page.svelte
+++ src/routes/posts/[slug]/+page.svelte
@@ -12,15 +12,13 @@
 
 <div class="container mx-auto max-w-xl px-4">
   <img class="pb-4" src={data.post.image.url} alt="im alt text" />
-  <h1
-    class="title-ch-limit text-4xl font-semibold text-gray-900 dark:text-white md:text-5xl xl:text-6xl"
-  >
+  <h1 class="title-ch-limit text-4xl font-semibold dark:text-white md:text-5xl xl:text-6xl">
     {data.post.title}
   </h1>
   <!-- <p>{data.post.excerpt}</p> -->
   <!-- <div class="prose">{data.post.main_content.html}</div> -->
   <!-- svelte-ignore missing-declaration -->
-  <div class="prose">{@html marked.parse(data.post.day_story)}</div>
+  <article class="prose prose-lg">{@html marked.parse(data.post.day_story)}</article>
   <article class="prose">
     {@html data.post.extra_rich_text.html}
   </article>
