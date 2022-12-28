a32a7e23f0cafd1cea2650617a085ffebbbdc53c
amy stout <asbilly92@gmail.com>
2022-12-27 19:17:36 -0500

    tried to revert

--- .netlify/functions-internal/render.mjs
+++ .netlify/functions-internal/render.mjs
@@ -6,7 +6,7 @@ export const handler = init({
 	assets: new Set(["favicon.png","start-line-no-kite.jpeg","tuesday-sailboat-outlined.svg","sailboat-hr.svg","sailboat-hr-test.svg","sailboat-hr-test-op.svg","gps-location-symbol-op.svg","map-marker-EZ.svg","start-line-op_hpda3d_c_scale,w_998.webp","start-line-op_hpda3d_c_scale,w_1080.webp","start-line-op_hpda3d_c_scale,w_320.webp","start-line-op_hpda3d_c_scale,w_638.webp","start-line-op_hpda3d_c_scale,w_834.webp"]),
 	mimeTypes: {".png":"image/png",".jpeg":"image/jpeg",".svg":"image/svg+xml",".webp":"image/webp"},
 	_: {
-		entry: {"file":"_app/immutable/start-b63cfe21.js","imports":["_app/immutable/start-b63cfe21.js","_app/immutable/chunks/index-953fe873.js","_app/immutable/chunks/singletons-37a0903c.js"],"stylesheets":[],"fonts":[]},
+		entry: {"file":"_app/immutable/start-a97a592d.js","imports":["_app/immutable/start-a97a592d.js","_app/immutable/chunks/index-953fe873.js","_app/immutable/chunks/singletons-37a0903c.js"],"stylesheets":[],"fonts":[]},
 		nodes: [
 			() => import('../server/nodes/0.js'),
 			() => import('../server/nodes/1.js'),
--- .netlify/server/index.js
+++ .netlify/server/index.js
@@ -2416,7 +2416,7 @@ class Server {
       app_template,
       app_template_contains_nonce: false,
       error_template,
-      version: "1672175885547"
+      version: "1672186502123"
     };
   }
   async init({ env }) {
