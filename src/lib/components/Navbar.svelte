d16de0f0cc293aa3e966352acc09588497d39011
amy stout <asbilly92@gmail.com>
2022-12-29 10:24:06 -0500

    fixed bottom links temporarily

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -6,7 +6,7 @@
   class="navbar flex-col bg-neutral text-neutral-content shadow-lg selection:rounded-b-lg sm:flex-row"
 >
   <div class="mx-2 px-2 lg:navbar-start">
-    <span class="text-md font-bold"><a href="/">Lake Erie Solo Challenge 2022</a> </span>
+    <span class="text-md font-bold"><a href="/">Amy's Lake Erie Solo Challenge 2022</a> </span>
   </div>
   <div class="navbar-center mx-2 px-2">
     <nav class="flex items-stretch">
@@ -16,8 +16,8 @@
             >{title}</a>
         {/each} -->
 
-      <a class="btn btn-ghost btn-sm rounded-btn" href="/">Home</a>
-      <a class="btn btn-ghost btn-sm rounded-btn" href="/posts">MY DAYS</a>
+      <a class="btn-ghost rounded-btn btn-sm btn" href="/">Home</a>
+      <a class="btn-ghost rounded-btn btn-sm btn" href="/posts">MY DAYS</a>
     </nav>
   </div>
   <div class="sm:navbar-end">
