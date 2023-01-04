d2ee9f3a547c6793bd24ec156c04bdc69bbc021a
amy stout <asbilly92@gmail.com>
2023-01-03 19:00:39 -0500

    adjusted h1 title on archive pages for dark mode

--- src/lib/components/Title.svelte
+++ src/lib/components/Title.svelte
@@ -2,7 +2,7 @@
   export let title = 'My Longest Sail';
 </script>
 
-<h1 class="text-center text-4xl font-semibold dark:text-white md:text-5xl xl:text-6xl">
+<h1 class="text-center text-4xl font-semibold md:text-5xl xl:text-6xl">
   {title}
 </h1>
 
