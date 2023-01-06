d1ac2d092a58fe1418876edbbb01f8797c7d3919
amy stout <asbilly92@gmail.com>
2023-01-06 11:42:52 -0500

    added google analytics to layout file

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,10 +5,28 @@
   import { themeChange } from 'theme-change';
   import '../app.css';
 
+  try {
+    if (typeof window !== 'undefined' && window) {
+      const googleAnalyticsId = 'G-31QPTK36FD';
+      window.dataLayer = window.dataLayer || [];
+      function gtag() {
+        window.dataLayer.push(arguments);
+      }
+      gtag('js', new Date());
+      gtag('config', googleAnalyticsId);
+    }
+  } catch (error) {
+    console.error('Failed to initialize Analytics');
+  }
+
   onMount(async () => {
     themeChange(false);
   });
 </script>
+
+<svelte:head>
+  <script async src="https://www.googletagmanager.com/gtag/js"></script>
+</svelte:head>
 
 <div id="scroll-to-here" />
 <Navbar />
