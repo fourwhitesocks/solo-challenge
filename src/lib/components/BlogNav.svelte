4bfe5f9c24106983f867adbe100be94952c3b9de
amy stout <asbilly92@gmail.com>
2023-03-02 09:33:06 -0500

    adjusted BlogNav size at med screens

--- src/lib/components/BlogNav.svelte
+++ src/lib/components/BlogNav.svelte
@@ -14,14 +14,30 @@
     class="pr-4 underline decoration-1 hover:underline-offset-8 sm:text-xl md:text-2xl"
     href={dayone}>Start</a
   >
-  <a class="pr-4 underline decoration-1 hover:underline-offset-8 sm:text-xl" href={daytwo}>2</a>
-  <a class="pr-4 underline decoration-1 hover:underline-offset-8 sm:text-xl" href={daythree}>3</a>
-  <a class="underine pr-4 decoration-1 hover:underline-offset-8 sm:text-xl" href={dayfour}>4</a>
-  <a class="pr-4 underline decoration-1 hover:underline-offset-8 sm:text-xl" href={dayfive}>5</a>
+  <a
+    class="pr-4 underline decoration-1 hover:underline-offset-8 sm:text-xl md:text-2xl"
+    href={daytwo}>2</a
+  >
+  <a
+    class="pr-4 underline decoration-1 hover:underline-offset-8 sm:text-xl md:text-2xl"
+    href={daythree}>3</a
+  >
+  <a
+    class="underine pr-4 decoration-1 hover:underline-offset-8 sm:text-xl md:text-2xl"
+    href={dayfour}>4</a
+  >
+  <a
+    class="pr-4 underline decoration-1 hover:underline-offset-8 sm:text-xl md:text-2xl"
+    href={dayfive}>5</a
+  >
 
-  <a class="pr-4 underline decoration-1 hover:underline-offset-8 sm:text-xl" href={daysix}>6</a>
-  <a class=" underline decoration-1 hover:underline-offset-8 sm:text-xl" href={conclusion}
-    >Conclusion</a
+  <a
+    class="pr-4 underline decoration-1 hover:underline-offset-8 sm:text-xl md:text-2xl"
+    href={daysix}>6</a
+  >
+  <a
+    class=" underline decoration-1 hover:underline-offset-8 sm:text-xl md:text-2xl"
+    href={conclusion}>Conclusion</a
   >
 </div>
 
